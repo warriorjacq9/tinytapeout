83c5bbed6c8cedfe43fee3d1bacbdaaef0559bdf
htfab <mpw@htamas.net>
2024-06-05 21:33:47 +0200

    feat: factory test module

--- test/tb.v
+++ test/tb.v
@@ -27,8 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // Replace tt_um_factory_test with your module name:
+  tt_um_factory_test user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
